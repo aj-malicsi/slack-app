3dce343e930d53aaa020d65e3f65f8a9c6201ef3
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-08-31 19:27:17 +0800

    stash for change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import RegisterForm from './components/registration/RegisterForm';
+import LoginForm from './components/Login/LoginForm';
 
 
 function App() {
   return (
+    <>
+
     <RegisterForm/>
+
+    <LoginForm/>
+    </>
   );
 }
 
--- src/components/registration/RegisterForm.js
+++ src/components/registration/RegisterForm.js
@@ -1,18 +1,60 @@
+
+import {useForm} from "react-hook-form";
+import axios from "axios";
+
 function RegisterForm(props){
 
+    const {register, handleSubmit, watch, errors} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data.email, data.password, data.conPass);
+        
+        axios.post("http://206.189.91.54/api/v1/auth", {
+            email: data.email,
+            password: data.password,
+            password_confirmation: data.conPass,
+            })
+            .then((response) => {
+                console.log("response",response);
+            })
+            .catch((error) => console.log("this is an error",error));
+        
+        };
+        
+
+
+
+    
+
     return(
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}> 
             <p>Register</p>
             <label>Enter Email:</label>
-            <input type="email" placeholder="example@mail.com"></input>
+            <input 
+            type="email" 
+            placeholder="Email"
+            {...register("email")}
+            />
             <br></br>
 
             <label>Enter Password:</label>
-            <input type="password"></input>
+            <input 
+            type="password" 
+            placeholder="Password"
+            {...register("password", {required:true}) }
+            required={true}
+            />
 
             <br></br>
+
             <label>Confirm Password:</label>
-            <input type="password"></input>
+            <input 
+            type="password"
+            placeholder="Confirm Password"
+            required={true}
+            {...register("conPass")}
+            />
+
             <br></br>
             <button type="submit">Register</button>
         </form>
