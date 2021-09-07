cbad5eca491cde554e576c06534947653d2f9ece
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2021-09-07 19:22:53 +0800

    fix infinite get request bug

--- src/components/Messaging/ChannelMessages.js
+++ src/components/Messaging/ChannelMessages.js
@@ -7,6 +7,7 @@ function ChannelMessages(props){
     //react hook form variables
 
     const [messages, setMessages] = useState([])
+    
 
     const {
         register,
@@ -40,21 +41,73 @@ function ChannelMessages(props){
         }
 
         // console.log(messageData)
-
-        axios
-            .post("http://206.189.91.54/api/v1/messages", messageData, {
+        
+        axios.post("http://206.189.91.54/api/v1/messages", messageData, {
                 headers: headersList,
             })
 
             .then((response) => {
                 console.log("POST RESPONSE")
-                console.log(response)    
+                console.log(response)
+                axios
+                    .get(`http://206.189.91.54/api/v1/messages?receiver_id=${props.id}&receiver_class=Channel`,{
+                        headers: headersList,
+                    })
+                    .then((response) =>{
+                        console.log("GET RESPONSE")
+                        console.log(response.data.data)
+                        setMessages(response.data.data)
+                        // console.log("messageArr",messageArr)
+                        
+
+                    })
+                    .catch( (error) => console.log(error.message) );    
 
             })
             .catch((error) => console.log(error.message));
 
-        }
+        
 
+    }
+        
+        // using component
+        // axios
+        //     .get(`http://206.189.91.54/api/v1/messages?receiver_id=${props.id}&receiver_class=Channel`,{
+        //         headers: headersList,
+        //     })
+        //     .then((response) =>{
+        //         console.log("GET RESPONSE")
+        //         console.log(response.data.data)
+        //         messageArr = response.data.data
+        //         setMessages(messageArr)
+        //         // console.log("messageArr",messageArr)
+                
+
+        //     })
+        //     .catch( (error) => console.log(error.message) );
+
+        useEffect( () =>{
+            axios
+            .get(`http://206.189.91.54/api/v1/messages?receiver_id=${props.id}&receiver_class=Channel`,{
+                headers: headersList,
+            })
+            .then((response) =>{
+                console.log("GET RESPONSE")
+                console.log(response.data.data)
+                messageArr = response.data.data
+                setMessages(messageArr)
+                // console.log("messageArr",messageArr)
+                
+
+            })
+            .catch( (error) => console.log(error.message) );
+
+        },[])
+
+        
+
+        //using array
+        
         
         // axios
         //     .get(`http://206.189.91.54/api/v1/messages?receiver_id=${props.id}&receiver_class=Channel`,{
@@ -62,29 +115,38 @@ function ChannelMessages(props){
         //     })
         //     .then((response) =>{
         //         console.log("GET RESPONSE")
+        //         console.log(response.data.data)
+        //         messageArr = response.data.data
         //         // console.log(response.data.data)
-        //         setMessages(response.data.data)
+        //         // setMessages(response.data.data)
         //         // console.log("messageArr",messageArr)
+
                 
 
         //     })
         //     .catch( (error) => console.log(error.message) );
       
 
-
-    // if(messageArr.length > 0){
-    //     console.log(messageArr[0])
-
-    // }
     
-    
-
         return(
             <>
             <div>channel messages</div>
             <ChannelDisplay
             messages={messages}
             />
+            {/* <ul>
+          {
+            messageArr.map((message) => {
+              return (
+                <> 
+                <div key={message.id}>
+                  {message.body} sent by {message.sender.uid}
+                </div>
+                </>
+              )
+            })
+          }
+        </ul> */}
     
     
             <form onSubmit={handleSubmit(onSubmit)}>
